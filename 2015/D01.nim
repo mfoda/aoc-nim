e30031b6f06d56dcf5b87df5d37fea8a15d272c0
mfoda <23182653+mfoda@users.noreply.github.com>
2021-01-13 20:21:48 +0800

    Add 2015/D01

---
+++ 2015/D01.nim
@@ -0,0 +1,19 @@
+import sequtils, sugar, strutils, strformat
+
+let input = readFile("2015/D01.txt")
+
+var currFloor = 0
+var basementIdx = 0
+for idx, direction in input:
+    currFloor += (case direction:
+      of '(': +1
+      of ')': -1
+        else: raise newException(ValueError, "invalid direction"))
+    if basementIdx == 0 and currFloor == -1:
+        basementIdx = idx
+
+# part 1
+echo &"Floor reached after following directions: {currFloor}"
+
+# part 2
+echo &"Position of direction leading to basement: {basementIdx + 1}"
